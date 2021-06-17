4fc1f5c7a14548e6b66cce793701b0a8dbdea72f
Marcus Vinicius <marcus15233v@gmail.com>
2021-06-17 18:49:50 -0300

    enviando o pulo do personagem

--- Game_Dapp/Assets/script/PlayerMoviment.cs
+++ Game_Dapp/Assets/script/PlayerMoviment.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class PlayerMoviment : MonoBehaviour
 {
-
     public float Speed;
+    public float JumpForce;
+    private Rigidbody2D rig;
 
     // Start is called before the first frame update
-    void Start()
+    void Start() //Executada apenas 1 vez
     {
-        
+        rig = GetComponent<Rigidbody2D>(); //controla a graviadde do personagem
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() //executa mais de uma vez
     {
         Move();
+        Jump();
     }
 
-    void Move()
+    void Move() //loica do moviemntoo do pergonagem
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Speed * Time.deltaTime;
+    }
+
+    void Jump() //logica do pulo do personagem
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+        }
     }
 }
