5744869e9f6a203b27007fa1fb550e2a36e5b7ae
Gabriel F <gfp001@outlook.com>
2021-06-18 02:56:55 -0300

    double jump, flip character with his direction and teleporter fix

--- Game_Dapp/Assets/script/PlayerMoviment.cs
+++ Game_Dapp/Assets/script/PlayerMoviment.cs
@@ -6,32 +6,86 @@ public class PlayerMoviment : MonoBehaviour
 {
     public float Speed;
     public float JumpForce;
+    public float forwardJumpForce;
+    public int allowedJumpsInAir;
+    private int jumpsInAir = 0;
     private Rigidbody2D rig;
+    private BoxCollider2D boxCollider;
+    private LayerMask groundLayer;
+    
 
     // Start is called before the first frame update
     void Start() //Executada apenas 1 vez
     {
+        groundLayer = LayerMask.GetMask("Solid");
         rig = GetComponent<Rigidbody2D>(); //controla a graviadde do personagem
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update() //executa mais de uma vez
     {
         Move();
-        Jump();
+
+        if (Input.GetButtonDown("Jump")) {
+            Jump();
+        }
+    }
+
+    float getDirection() {
+        return Mathf.Sign(transform.localScale.x);
     }
 
     void Move() //loica do moviemntoo do pergonagem
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        float inputHorizontal = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(inputHorizontal) > 0.01f)
+        {
+            transform.localScale = new Vector3(
+                Mathf.Sign(inputHorizontal) * Mathf.Abs(transform.localScale.x), 
+                transform.localScale.y,
+                transform.localScale.z
+            );
+        }
+
+        Vector3 movement = new Vector3(inputHorizontal, 0f, 0f);
         transform.position += movement * Speed * Time.deltaTime;
+    }
+
+    bool isGrounded() {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(
+            boxCollider.bounds.center,
+            boxCollider.bounds.size,
+            0,
+            Vector2.down,
+            0.1f,
+            groundLayer
+        );
+        return raycastHit.collider != null;
     }
 
     void Jump() //logica do pulo do personagem
     {
-        if (Input.GetButtonDown("Jump"))
-        {
-            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+        bool shouldJump = false;
+
+        if (this.isGrounded()) {
+            this.jumpsInAir = 0;
+
+            shouldJump = true;
+        } else {
+            if (jumpsInAir < allowedJumpsInAir) {
+                this.jumpsInAir++;
+
+                shouldJump = true;
+            }
+        }
+
+        if (shouldJump) {
+            rig.AddForce(
+                new Vector2(this.getDirection() * forwardJumpForce, JumpForce), 
+                ForceMode2D.Impulse
+            );
         }
     }
 }
--- Game_Dapp/Assets/script/TrocaScene.cs
+++ Game_Dapp/Assets/script/TrocaScene.cs
@@ -6,10 +6,11 @@ using UnityEngine.SceneManagement;
 public class TrocaScene : MonoBehaviour
 {
     public int SceneValue;
+    BoxCollider2D collider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -20,6 +21,15 @@ public class TrocaScene : MonoBehaviour
     private void OnMouseDown()
     {
         SceneManager.LoadScene(SceneValue);
+    }
+    void OnCollisionEnter2D(Collision2D coll) {
+        print("touching");
+        if (coll.gameObject.tag == "Player")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            print("Player touching");
+            SceneManager.LoadScene(SceneValue);
+        }
     }
 
 }
